3c995c8a978e56185e9b7fd777c7e3c1d05d0001
Jonathan Boller <5533@holbertonstudents.com>
2024-01-04 21:56:34 -0600

    Update before switich projects.

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
+
+find_num = /\[\w+:\+([0-9]{10,})\]/
+find_flags = /\[flags:([^\[\]]+)\]/
+
+test_string = "Feb  1 11:00:10 ip-10-0-64-11 mdr: 2016-02-01 11:00:10 Receive SMS [SMSC:SYBASE1] [SVC:] [ACT:] [BINF:] [FID:] [from:+17097700643] [to:+17097004811] [flags:-1:0:-1:0:-1] [msg:136:Far out in the uncharted backwaters of the unfashionable end of the western spiral arm of the Galaxy lies a small unregarded yellow sun.] [udh:0:]"
+
+nums = test_string.scan(find_flags)
+
+puts nums
